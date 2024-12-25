34a954e981a40b51eade13b5c5c03439c1577c2c
zanilos001 <ali.elkadiri@elaraki.ac.ma>
2024-12-25 19:34:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 clientId: "", // id bot
     guildId: "", // id server
-    token: "" //token bot
+    token: "MTMyMTU0NTA5NzU0MzY4MDAzMA.GYh2gF.Gpp7Ji4A-M4pi3qwTlaC7lGKXG0i2GJtOGMYMg" //token bot
 }
